b3fdce7e2483ca3928d73d056f32331902ae3551
Andriy Vlonha <andriy.vlonha.dev@gmail.com>
2026-07-21 14:00:00 +0300

    feat: add networking module
    VNet with three subnets (aks, app, private-endpoint), one NSG per subnet, and private DNS zones for privatelink endpoints.

---
+++ terraform/modules/networking/main.tf
@@ -0,0 +1,45 @@
+﻿resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  address_space       = var.vnet_address_space
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  for_each             = var.subnets
+  name                 = "snet-${each.key}"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = each.value.address_prefixes
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  for_each            = var.subnets
+  name                = "nsg-${each.key}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  tags = var.tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  for_each                  = var.subnets
+  subnet_id                 = azurerm_subnet.subnet[each.key].id
+  network_security_group_id = azurerm_network_security_group.nsg[each.key].id
+}
+
+resource "azurerm_private_dns_zone" "privatelink" {
+  for_each            = var.private_dns_zones
+  name                = each.value
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "privatelink_link" {
+  for_each              = var.private_dns_zones
+  name                  = "${replace(each.value, ".", "-")}-link"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.privatelink[each.key].name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
---
+++ terraform/modules/networking/variables.tf
@@ -0,0 +1,42 @@
+﻿variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space of the virtual network"
+  type        = list(string)
+}
+
+variable "subnets" {
+  description = "Map of subnets: name -> { address_prefixes }"
+  type = map(object({
+    address_prefixes = list(string)
+  }))
+}
+
+variable "private_dns_zones" {
+  description = "Private DNS zones to create (privatelink.*)"
+  type        = map(string)
+  default = {
+    postgres = "privatelink.postgres.database.azure.com"
+    keyvault = "privatelink.vaultcore.azure.net"
+    blob     = "privatelink.blob.core.windows.net"
+  }
+}
+
+variable "tags" {
+  description = "Tags applied to all resources"
+  type        = map(string)
+  default     = {}
+}
