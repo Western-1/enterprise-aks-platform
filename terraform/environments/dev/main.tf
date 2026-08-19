b822bbf317b10fa2daafe1aad250012a91a38df2
Andriy vlonha <andriy.vlonha.dev@gmail.com>
2026-08-19 16:45:52 +0300

    feat: add Key Vault module with private endpoint and RBAC
    Vault kv-dev-media-ne: RBAC authorization, access only via private endpoint, DNS zone privatelink.vaultcore.azure.net. The current user is granted the Key Vault Administrator role.

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -53,3 +53,16 @@ module "acr" {
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
 }
+
+module "key_vault" {
+  source = "../../modules/key-vault"
+
+  name                       = "kv-dev-media-ne"
+  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.rg.location
+  tenant_id                  = "e6b52a20-0ba0-4ffd-951d-183b04425ed2"
+  current_user_object_id     = "6489c989-ee54-4e99-a2ad-f60a1acd493d"
+  allow_public               = false
+  private_endpoint_subnet_id = module.networking.subnet_ids["private-endpoint"]
+  private_dns_zone_id        = module.networking.private_dns_zone_ids["keyvault"]
+}
---
+++ terraform/modules/key-vault/variables.tf
@@ -0,0 +1,47 @@
+variable "name" {
+  description = "Key Vault name"
+  type        = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "current_user_object_id" {
+  description = "Object ID of the current user (granted Key Vault Administrator)"
+  type        = string
+}
+
+variable "sku" {
+  type    = string
+  default = "standard"
+}
+
+variable "allow_public" {
+  description = "When false, the vault is reachable only via private endpoint"
+  type        = bool
+  default     = false
+}
+
+variable "private_endpoint_subnet_id" {
+  type    = string
+  default = null
+}
+
+variable "private_dns_zone_id" {
+  type    = string
+  default = null
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
